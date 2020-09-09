eb827c88d7f85f18ce0a2c9612c2f0222b51e59e
ananiy <ananiy@qq.com>
2020-09-09 21:26:16 +0800

    add app memo

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,22 @@ import { Counts } from "./components/count";
 import { Users } from "./components/user";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  const MemoCounts = React.useMemo(() => <Counts />, []);
+  const MemoUsers = React.useMemo(() => <Users />, []);
   return (
     <div className="App">
-      <Counts />
-      <Users />
+      <div>{count}</div>
+      <button
+        onClick={() => {
+          // 会触发 App 组件重新渲染
+          setCount((v) => v + 1);
+        }}
+      >
+        add
+      </button>
+      {MemoCounts}
+      {MemoUsers}
     </div>
   );
 }
